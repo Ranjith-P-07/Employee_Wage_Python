7a44b1950dd0e9ff6b9efb108f062d778a90b865
“Ranjith” <Ranjithroy704@gmail.com>
2020-11-30 18:41:16 +0530

    Add:Employee Attendence Check

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,2 +1,23 @@
 # Created by Ranjith P at 30-11-2020
-print("Welcome to Employee Wage Program")
+import random
+
+
+class Employee:
+
+    def employeeAttendance(self):
+        '''
+        This is a Employee Attendence checking method
+        :return:Present or Absent
+        :rtype: str
+        '''
+        check = random.randint(1, 2)
+        if check == 1:
+            return "Present"
+        else:
+            return "Absent"
+
+
+if __name__ == "__main__":
+    print("Welcome to Employee Wage Program")
+    emp = Employee()
+    print(emp.employeeAttendance())
