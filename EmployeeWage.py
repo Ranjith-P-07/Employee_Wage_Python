dcfb5a323456419cfdbc346fa1727159dea6fe1a
“Ranjith” <Ranjithroy704@gmail.com>
2020-11-30 20:12:05 +0530

    Add: Calculated Employee Monthly Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -6,8 +6,8 @@ class Employee:
     part_time_emp_hr = 4
     full_time_emp_hr = 8
     emp_rate_per_hr = 20
+    emp_hour = 0
     absent = 0
-    check = random.randint(0, 2)
 
     def employee_attendance(self):
         '''
@@ -15,40 +15,29 @@ class Employee:
         :return:Present or Absent
         :rtype:str
         '''
-        if Employee.check == 1:
+        check = random.randint(0, 2)
+        if check == 1:
+            Employee.emp_hour = 8
             return "Full Time Employee is Present"
-        elif Employee.check == 2:
+        elif check == 2:
+            Employee.emp_hour = 4
             return "Part Time Employee is Present"
         else:
+            Employee.emp_hour = 0
             return "Absent"
 
-    def getemp_working_hr(self):
-        '''
-        This method is Used to get Employee Working Hours
-        :return: Employee Working Hour
-        :rtype: int
-        '''
-        workHour = {0: Employee.absent, 1: Employee.full_time_emp_hr, 2: Employee.part_time_emp_hr}
-        if Employee.check == 1:
-            return workHour.get(1)
-        elif Employee.check == 2:
-            return workHour.get(2)
-        else:
-            return workHour.get(0)
-
-    def calculate_daily_emp_wage(self, empHr):
-        '''
-        This method is to calculate Daily Employee Wage
-        :param empHr: Employee Working Hour
-        :type empHr: int
-        :return: Daily Wage of Employees
-        :rtype: int
-        '''
-        return Employee.emp_rate_per_hr * empHr
+    def calculate_daily_emp_wage(self):
+        total_salary = 0
+        for day in range(1,20):
+            self.employee_attendance()
+            daily_wage = Employee.emp_rate_per_hr * Employee.emp_hour
+            total_salary = total_salary + daily_wage
+            print(f"Employee Daily Wage is : {daily_wage}")
+        return print(f"Total Wage for Month is : {total_salary}")
 
 
 if __name__ == "__main__":
     print("Welcome to Employee Wage Program")
     emp = Employee()
     print(emp.employee_attendance())
-    print(emp.calculate_daily_emp_wage(emp.getemp_working_hr()))
+    print(emp.calculate_daily_emp_wage())
