dbef2350bcce90a7ccfb818fc45668bf6f255ca0
“Ranjith” <Ranjithroy704@gmail.com>
2020-11-30 18:59:59 +0530

    Add:Calculation of Employee Wage on Daily Basis

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,21 +3,37 @@ import random
 
 
 class Employee:
+    check = random.randint(0, 1)
 
-    def employeeAttendance(self):
+    def employee_attendance(self):
         '''
         This is a Employee Attendence checking method
         :return:Present or Absent
         :rtype: str
         '''
-        check = random.randint(1, 2)
-        if check == 1:
+        if Employee.check == 1:
             return "Present"
         else:
             return "Absent"
+
+    def emp_daily_wage(self):
+        '''
+        This is to Calculate Daily Wage Of Employee
+        :return: salary
+        :rtype: int
+        '''
+        if Employee.check == 1:
+            emprateperhour = 20
+            empworkinghour = 8
+            salary = emprateperhour * empworkinghour
+            return salary
+        else:
+            salary = 0
+            return salary
 
 
 if __name__ == "__main__":
     print("Welcome to Employee Wage Program")
     emp = Employee()
-    print(emp.employeeAttendance())
+    print(emp.employee_attendance())
+    print(emp.emp_daily_wage())
