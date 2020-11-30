b71662de46bbc56e37457da13a0fa6faa6931d0a
“Ranjith” <Ranjithroy704@gmail.com>
2020-11-30 19:25:20 +0530

    Add: Refactored Code Using SwitchCase(using Dictionary)

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,14 +3,17 @@ import random
 
 
 class Employee:
+    part_time_emp_hr = 4
+    full_time_emp_hr = 8
+    emp_rate_per_hr = 20
+    absent = 0
     check = random.randint(0, 2)
-    emp_rate_per_hour = 20
 
     def employee_attendance(self):
         '''
         This is a Employee Attendence checking method
         :return:Present or Absent
-        :rtype: str
+        :rtype:str
         '''
         if Employee.check == 1:
             return "Full Time Employee is Present"
@@ -19,27 +22,33 @@ class Employee:
         else:
             return "Absent"
 
-    def emp_daily_wage(self):
+    def getemp_working_hr(self):
         '''
-        This is to Calculate Daily Wage Of Employee
-        :return: salary
+        This method is Used to get Employee Working Hours
+        :return: Employee Working Hour
         :rtype: int
         '''
+        workHour = {0: Employee.absent, 1: Employee.full_time_emp_hr, 2: Employee.part_time_emp_hr}
         if Employee.check == 1:
-            empworkinghour = 8
-            salary = Employee.emp_rate_per_hour * empworkinghour
-            return salary
+            return workHour.get(1)
         elif Employee.check == 2:
-            empworkinghour = 4
-            salary = Employee.emp_rate_per_hour * empworkinghour
-            return salary
+            return workHour.get(2)
         else:
-            salary = 0
-            return salary
+            return workHour.get(0)
+
+    def calculate_daily_emp_wage(self, empHr):
+        '''
+        This method is to calculate Daily Employee Wage
+        :param empHr: Employee Working Hour
+        :type empHr: int
+        :return: Daily Wage of Employees
+        :rtype: int
+        '''
+        return Employee.emp_rate_per_hr * empHr
 
 
 if __name__ == "__main__":
     print("Welcome to Employee Wage Program")
     emp = Employee()
     print(emp.employee_attendance())
-    print(emp.emp_daily_wage())
+    print(emp.calculate_daily_emp_wage(emp.getemp_working_hr()))
